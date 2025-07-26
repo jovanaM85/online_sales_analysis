fe01b45c6dcff76d208673dcbccfaa8953d0407e
JovanaM85 <maksjovana@gmail.com>
2025-07-26 20:18:09 +0200

    Promena Proizvoda!!

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from product_manager import ProductManager
 from product import Product
 
 pm = ProductManager()
-pm.add_product(Product("Tastatura",10,15))
-pm.add_product(Product("Mis",5,55))
-pm.add_product(Product("Monitor",100,70))
-pm.print_products()
-pm.calculate_quality()
-pm.remove_product("Monitor")
-pm.print_products()
+pm.add_product(Product("Tastatura",10,3))
+pm.add_product(Product("Mis",10,55))
+pm.add_product(Product("Monitor",100,22))
